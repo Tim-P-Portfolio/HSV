cd1c7d04f1b732ea7d046b0db87b36b6670067f7
tmpk13 <>
2026-03-11 02:08:20 -0700

    Adding steps

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 use cortex_m_rt::entry;
 use critical_section_lock_mut::LockMut;
-use embedded_hal::delay::DelayNs;
+use embedded_hal::{delay::DelayNs, digital::OutputPin};
 use hsv::*;
 use microbit::{
     board::Board,
@@ -19,8 +19,7 @@ use rtt_target::{rprintln, rtt_init_print};
 const FRAME_TICKS: u32 = 256;
 const TICK_US: u32 = 50;
 
-
-
+// Source used to help with implementation: https://docs.rust-embedded.org/discovery-mb2/15-interrupts/my-solution.html
 struct RgbDisplay {
     tick: u32,
     schedule: [u32; 3],
@@ -40,6 +39,7 @@ impl RgbDisplay {
             timer,
         }
     }
+
     fn start(&mut self) {
         self.tick = 0;
         self.timer.enable_interrupt();
@@ -50,9 +50,34 @@ impl RgbDisplay {
         self.timer.disable_interrupt();
     }
 
+    fn set(&mut self, hsv: &Hsv) {
+        let rgb = hsv.to_rgb();
+        // Schedule with rgb value
+        self.next_schedule = Some([
+            (rgb.r * 255.0) as u32,
+            (rgb.g * 255.0) as u32,
+            (rgb.b * 255.0) as u32,
+        ]);
+    }
 
-    fn set(&mut self, hsv: &Hsv) {}
-    fn step(&mut self) {}
+    fn step(&mut self) {
+        if self.tick == 0 {
+            if let Some(s) = self.next_schedule.take() {
+                self.schedule = s;
+            }
+            for pin in &mut self.rgb_pins {
+                let _ = pin.set_high();
+            }
+        }
+        for (i, pin) in self.rgb_pins.iter_mut().enumerate() {
+            if self.tick >= self.schedule[i] {
+                let _ = pin.set_low();
+            }
+        }
+        self.tick = (self.tick + 1) % FRAME_TICKS;
+        self.timer.reset_event();
+        self.timer.start(TICK_US);
+    }
 }
 
 static DISPLAY: LockMut<RgbDisplay> = LockMut::new();
